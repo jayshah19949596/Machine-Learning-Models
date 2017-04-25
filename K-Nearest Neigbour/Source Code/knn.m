ddf1b681a63e98a002689e7b68cdadbb961e8fe7
Jai Shah <jai.s@somaiya.edu>
2017-04-25 12:40:02 -0500

    Update knn.m

--- K-Nearest Neigbour/Source Code/knn.m
+++ K-Nearest Neigbour/Source Code/knn.m
@@ -1,14 +1,21 @@
 function [] = knn(train_data, test_data, train_target, test_target, k)
-    %disp(train_data(1, :));
-    %disp(test_data(1, :));
     classification_accuracy = 0;
     for i = 1:size(test_data, 1)
+        %============================================
+        %         Calculating Euclidean Distance
+        %============================================
         D = test_data(i, :) - train_data(: , :);
         D = D.^2;
         dist_mat = sum(D, 2);
         dist_mat = sqrt(dist_mat);
         dist = [dist_mat train_target];
+        %============================================
+        %  Sorting Row according to minimum distance
+        %============================================
         dist = sortrows(dist, 1);
+        %============================================
+        %       If K value is 1 Print Results
+        %============================================
         if k == 1
             k_neighbours = dist(k, :);
             predicted = k_neighbours(1, 2);
@@ -20,6 +27,9 @@ function [] = knn(train_data, test_data, train_target, test_target, k)
                 accuracy = 0;
             end
             fprintf('ID=%5d, predicted=%3d, true=%3d, accuracy=%4.2f \n', i, predicted, true, accuracy)
+        %===============================================
+        % Else K value is greater then 1 Print Results
+        %===============================================
         else
             k_neighbours = dist(1:k, :);            
             if size(unique(k_neighbours(:, 2))) == 1
@@ -27,11 +37,7 @@ function [] = knn(train_data, test_data, train_target, test_target, k)
             elseif size(unique(k_neighbours(:, 2))) == k
                 predicted = k_neighbours(1, 2);
             else
-                predicted = mode(k_neighbours(:, 2));
-                %disp(transpose(k_neighbours(:, 2)))
-                %disp(count);
-                %count = sortrows(dist, 1);
-                %predicted = count(end, end);
+                predicted = mode(k_neighbours(:, 2))
             end
             true = test_target(i, 1);
             if true == predicted
